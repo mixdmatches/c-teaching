6f72f4247ac819250dc0be5fff886cbf68e5c0fd
mixed <1919600547@qq.com>
2025-03-02 00:06:06 +0800

    commit "建立api文件夹，统一api规则"

---
+++ src/api/user.js
@@ -0,0 +1,5 @@
+// 比如这里放用户相关的接口函数
+import service from '../utils/request'
+// 所有接口函数都要以api开头，请求类型，后面跟接口名称，命名最好语义化，统一用箭头函数
+// export const apiGetUserInfo = () => service.get('/user/info')
+// export const apiPostUserInfo = data => service.post('/user/info', data)
