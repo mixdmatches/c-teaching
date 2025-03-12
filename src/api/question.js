4d423756e510e7cd10816de7a7d999af9468dced
dsy <dsy214495@qq.com>
2025-03-12 18:44:03 +0800

    commit -m "冲突解决"

--- src/api/question.js
+++ src/api/question.js
@@ -1,10 +1,10 @@
 import service from "@/utils/request.js";
-export const getQuestionBySectionId = (params) => {
-  return service.get('/api/topic/section',{params})
+export const getQuestionBySectionId = async (params) => {
+  return (await service.get('/api/topic/section', { params })).data
 }
-export const getQuestionByKnowledge = (data) => {
-  return service.post('/api/topic/knowledge',data)
+export const getQuestionByKnowledge = async (data) => {
+  return (await service.post('/api/topic/knowledge', data)).data
 }
-export const getAnswer = (data) => {
-  return service.post('/api/topic/result',data)
+export const getAnswer = async (data) => {
+  return (await service.post('/api/topic/result', data)).data
 }
