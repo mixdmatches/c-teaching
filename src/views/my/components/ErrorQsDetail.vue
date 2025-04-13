63379c49f14b3440687bb861a33df37bcea65c93
mixed <1919600547@qq.com>
2025-04-13 14:20:53 +0800

    feat(错题本): 新增错题本功能并优化代码结构
    新增错题本功能，包括错题列表展示、错题详情弹窗、章节筛选等功能。优化代码结构，移除冗余代码，提升代码可读性和维护性。

---
+++ src/views/my/components/ErrorQsDetail.vue
@@ -0,0 +1,151 @@
+<template>
+  <el-dialog v-model="visible" title="题目详情" width="800">
+    <div class="questionItem border">
+      <div class="title">{{ currentRow.title }}</div>
+      <ul class="options" v-for="op in currentRow.option" :key="op.key">
+        <li
+          class="option"
+          :class="{
+            errorOption: op.key == currentRow.studentAnswer,
+            trueOption: op.key == currentRow.answer,
+          }"
+        >
+          {{ op.key }}、{{ op.value }}
+        </li>
+      </ul>
+      <div class="result">
+        <span class="true-ans">正确答案: {{ currentRow.answer }}</span>
+        <span class="err-ans"
+          >你的答案:
+          {{
+            currentRow.studentAnswer ? currentRow.studentAnswer : '未作答'
+          }}</span
+        >
+        <span class="jiexi"
+          >解析：
+          <p>
+            {{ currentRow.analysis }}
+          </p>
+        </span>
+      </div>
+    </div>
+  </el-dialog>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed, toRef } from 'vue'
+const props = defineProps({
+  visible: Boolean,
+  currentRow: Object,
+})
+const currentRow = toRef(props, 'currentRow')
+const emits = defineEmits(['update:visible'])
+const visible = computed({
+  get: () => props.visible,
+  set: value => emits('update:visible', value),
+})
+</script>
+
+<style lang="scss" scoped>
+.border {
+  border: 1px solid $light-border-color;
+  border-radius: 4px;
+}
+.errorQuestions {
+  margin-top: $margin-xl;
+}
+.questionItem {
+  // width: 1200px;
+  // height: 400px;
+  // margin-top: $margin-s;
+  background-color: $base-bg-color;
+  border-radius: $border-radius-m;
+  padding: $padding-xl;
+  .info {
+    display: flex;
+    align-items: center;
+    gap: $padding-xxl;
+    .type {
+      background-color: $primary-color;
+      color: white;
+      font-size: $font-size-l;
+      padding: $padding-s;
+      border-radius: 9999px;
+    }
+    .starBox {
+      display: flex;
+      align-items: center;
+      .label {
+        color: $primary-color;
+        font-size: $font-size-l;
+      }
+    }
+    .tagBox {
+      display: flex;
+      align-items: center;
+      gap: $padding-s;
+    }
+  }
+  .title {
+    margin-bottom: $padding-l;
+    color: $primary-color;
+    font-size: $font-size-xxl;
+  }
+  .options {
+    display: flex;
+    flex-direction: column;
+    .option {
+      padding: $padding-m;
+      border: 1px solid $light-border-color;
+      border-radius: $border-radius-m;
+      margin-bottom: $margin-s;
+    }
+  }
+  .trueOption {
+    background-color: $success-color;
+    color: white;
+  }
+  .errorOption {
+    background-color: $danger-color;
+    color: white;
+  }
+  .radioGroup {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    padding-left: 20px;
+  }
+}
+.submit {
+  position: absolute;
+  top: 270px;
+  left: 40px;
+}
+.result {
+  margin-top: $margin-l;
+  font-size: $font-size-xl;
+  display: flex;
+  flex-direction: column;
+  gap: $padding-s;
+  .true-ans {
+    color: $success-color;
+    font-weight: 500;
+    margin-bottom: $margin-s;
+  }
+  .err-ans {
+    color: $danger-color;
+    font-weight: 500;
+  }
+  .jiexi {
+    line-height: 1.5;
+    margin-top: $margin-l;
+    font-weight: 500;
+    color: $primary-color;
+    p {
+      font-size: $font-size-l;
+      font-weight: 400;
+      color: $info-color;
+    }
+  }
+}
+</style>
--- src/views/my/components/ErrorQustion.vue
+++ src/views/my/components/ErrorQustion.vue
@@ -4,92 +4,118 @@
     <template #header>
       <div class="header">
         <span>错题本</span>
-        <span>共10题</span>
+        <span>共{{ errorQuestionList.length }}题</span>
       </div>
     </template>
-    <el-select style="width: 200px" v-model="selectOption">
-      <option
+    <el-select
+      style="width: 200px"
+      @change="handleChangeChapter"
+      v-model="selectOption"
+    >
+      <el-option
+        v-for="item in chapter"
         :label="item.text"
         :value="item.id"
-        v-for="item in chapter"
-        :key="item"
-      ></option>
+        :key="item.id"
+      ></el-option>
     </el-select>
-    <!-- 错题列表 -->
-    <div class="errorQuestions">
-      <!-- 单题 -->
-      <div class="questionItem border" v-for="item in 4" :key="item">
-        <div class="info">
-          <div class="type">单选题</div>
-          <div class="starBox">
-            <div class="label">难度：</div>
-            <Stars :num="3" />
-          </div>
-          <div class="starBox">
-            <div class="label">重点：</div>
-            <Stars :num="3" />
-          </div>
-          <div class="tagBox">
-            <Tag text="指针" />
-          </div>
-          <el-button>做同类型题</el-button>
-        </div>
-        <div class="title">1. c语言是什么</div>
-        <el-radio-group v-model="model" class="radioGroup">
-          <el-radio
-            v-for="(item, index) in options"
-            :key="index"
-            :label="item.id + '、' + item.text"
-            :value="item.id"
-          />
-        </el-radio-group>
-        <div class="result">
-          <span class="true-ans">正确答案:A</span>
-          <span class="jiexi"
-            >解析：
-            <p>
-              c语言是一种通用的、过程式的计算机程序设计语言。
-              1972年，为了移植与开发UNIX操作系统，丹尼斯·里奇在贝尔电话实验室设计开发了C语言。
-              C语言是一种广泛使用的计算机编程语言，它提供了高级程序设计语言的一些特性，
-              允许开发者以更接近硬件的方式编写程序。
-            </p>
-          </span>
-        </div>
-      </div>
-    </div>
+    <el-table :data="errorQuestionList" style="width: 100%" height="300">
+      <el-table-column prop="title" label="题目" />
+      <el-table-column prop="knowPointName" label="知识点" width="180">
+        <template #default="scope">
+          <el-tag>{{ scope.row.knowPointName }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="difficulty" label="难度">
+        <template #default="scope">
+          <el-rate disabled v-model="scope.row.difficulty" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="hierarchy" label="重点">
+        <template #default="scope">
+          <el-rate disabled v-model="scope.row.hierarchy" />
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template #default="{ row }">
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="handleVisibelity(row)"
+          >
+            查看详情
+          </el-button>
+          <el-button link type="primary" size="small">做同类型</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <ErrorQsDetail
+      :visible="dialogTableVisible"
+      :currentRow="currentRow"
+      @update:visible="dialogTableVisible = $event"
+    />
   </el-card>
 </template>
 
 <script setup>
-import Stars from '@/components/Stars.vue'
-import Tag from '@/components/Tag.vue'
-import { ref } from 'vue'
-const selectOption = ref('全部')
-const options = [
-  { id: 'A', text: '高级语言' },
-  { id: 'B', text: '低级语言' },
-  { id: 'C', text: '机器语言' },
-  { id: 'D', text: '汇编语言' },
-]
+// 引入api
+import { getErrorQuestion } from '@/api/question'
+// 引入组件
+import ErrorQsDetail from './ErrorQsDetail.vue'
+// 引入仓库
+import { useUserStore } from '@/stores/index'
+// 引入hook
+import { onMounted, ref } from 'vue'
+
+const userStore = useUserStore()
+// 章节选择变量
+const selectOption = ref(1)
+//控制弹窗显隐变量
+const dialogTableVisible = ref(false)
+//当前查看的行数据
+const currentRow = ref({})
+// 章节列表变量
 const chapter = [
-  { id: '1', text: '第一章' },
-  { id: '2', text: '第二章' },
-  { id: '3', text: '第三章' },
-  { id: '4', text: '第四章' },
-  { id: '5', text: '第五章' },
-  { id: '6', text: '第六章' },
-  { id: '7', text: '第七章' },
-  { id: '8', text: '第八章' },
-  { id: '9', text: '第九章' },
-  { id: '10', text: '第十章' },
+  { id: 1, text: '第一章' },
+  { id: 2, text: '第二章' },
+  { id: 3, text: '第三章' },
+  { id: 4, text: '第四章' },
+  { id: 5, text: '第五章' },
+  { id: 6, text: '第六章' },
+  { id: 7, text: '第七章' },
+  { id: 8, text: '第八章' },
+  { id: 9, text: '第九章' },
+  { id: 10, text: '第十章' },
 ]
+// 错题列表变量
+const errorQuestionList = ref([])
+// 获取错题列表函数
+const getErrorQuestionList = async () => {
+  const {
+    data: { showTopicResults },
+  } = await getErrorQuestion({
+    stuId: userStore.studentId,
+    sectionId: selectOption.value,
+  })
+  errorQuestionList.value = showTopicResults
+}
+// 选择章节变化函数
+const handleChangeChapter = () => {
+  getErrorQuestionList()
+}
+// 查看详情函数
+const handleVisibelity = row => {
+  dialogTableVisible.value = true
+  currentRow.value = row
+}
+// 生命周期函数 挂载后获取错题列表
+onMounted(() => {
+  getErrorQuestionList()
+})
 </script>
 
 <style lang="scss" scoped>
-.border {
-  border: 1px solid $light-border-color;
-  border-radius: 4px;
-}
 .header {
   display: flex;
   gap: $margin-s;
@@ -97,80 +123,6 @@ const chapter = [
     font-size: 1.6rem;
     font-weight: 500;
     color: $primary-color;
-  }
-}
-.errorQuestions {
-  margin-top: $margin-xl;
-}
-.questionItem {
-  // width: 1200px;
-  // height: 400px;
-  margin-top: $margin-s;
-  background-color: $base-bg-color;
-  border-radius: $border-radius-m;
-  padding: $padding-xl;
-  .info {
-    display: flex;
-    align-items: center;
-    gap: $padding-xxl;
-    .type {
-      background-color: $primary-color;
-      color: white;
-      font-size: $font-size-l;
-      padding: $padding-s;
-      border-radius: 9999px;
-    }
-    .starBox {
-      display: flex;
-      align-items: center;
-      .label {
-        color: $primary-color;
-        font-size: $font-size-l;
-      }
-    }
-    .tagBox {
-      display: flex;
-      align-items: center;
-      gap: $padding-s;
-    }
-  }
-  .title {
-    margin: $padding-xl 0;
-    color: $primary-color;
-    font-size: $font-size-xxl;
-  }
-  .radioGroup {
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    padding-left: 20px;
-  }
-}
-.submit {
-  position: absolute;
-  top: 270px;
-  left: 40px;
-}
-.result {
-  margin-top: $margin-l;
-  font-size: $font-size-xl;
-  display: flex;
-  flex-direction: column;
-  gap: $padding-s;
-  .true-ans {
-    color: $success-color;
-    font-weight: 500;
-  }
-  .jiexi {
-    line-height: 1.5;
-    margin-top: $margin-l;
-    font-weight: 500;
-    color: $primary-color;
-    p {
-      font-size: $font-size-l;
-      font-weight: 400;
-      color: $info-color;
-    }
   }
 }
 </style>
